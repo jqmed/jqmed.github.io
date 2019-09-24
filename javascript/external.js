5457f277daaea9f419b4f625ce479ede81539904
Medina <jqm4@txstate.edu>
2019-09-23 19:39:47 -0500

    Completed Task 3. Variables.

--- javascript/external.js
+++ javascript/external.js
@@ -1,8 +1,18 @@
-//document.write("Where is my JS code?");
+/*document.write("Where is my JS code?");
 
-//window.alert(5 + 6);
+window.alert(5 + 6);
 
-//window.alert('5 + 6');
+window.alert('5 + 6');
 
 document.write('<h1>A heading</h1>');
 document.write('<p>A sentence.</p>');
+
+
+const x = "web";
+const y = "mapping";
+const out = x + y;
+document.write(out);
+*/
+
+//A prompt box is used to prompt users to input
+a value before entering a page.
