d01b2a0fcb1630a6b58e60679ec4de199ecaed3f
Jeremy Medina <jqm4@txstate.edu>
2019-10-07 16:39:42 -0500

    Filled conted from webmaps arrays... This step broke my site. No propt.

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,15 @@
+var webmaps =
+  [
+    ["NowCOAST",
+    "https://nowcoast.noaa.gov/",
+    "A versatile tool for researchers, mariners and coastal residents alike. The default map screen is shown below. On the right side of the page there are a series of toolbars that control what is being projected onto the map including remotely sensed weather imagery and radar reflectivity. Also shown is the sites capability to track hurricanes by date, in this case Hurricane Dorian. While lacking in heavy statistical capabilities, the site provides users a customizable display of the U.S. coastline based on the parameters and extent of their choosing with the ability to return the values and qualities of selected variables by simply clicking on the point of interest. If I could recreate this site, the only change I could see making is the addition of a “NowCOAST for Research” page, similar to the NowCOAST for Mariners page visible on the top right of the page, which would be able to conduct some basic statistical tests of selected variables, though I do not know how that would be cleanly implemented."
+    ],
+    ["Earthquakes, Eruptions and Emissions",
+    "https://volcano.si.axismaps.io/",
+    "This interactive map produced by the Smithsonian displays basically what is in the title; recorded earthquakes, volcanic eruptions and high concentration of SO2 emissions over the course of almost 60 years. The user can select which types of events to focus on and even view the details of each event my mousing over the earthquake or eruption and clicking on any of these events will link you directly to the USGS page detailing the quake or eruption. The display can be changed to a globe view, which provides an interesting visual, and the speed of the time lapse can be changed to the user’s liking. What I found interesting is visualizing the order of events. Predictably, many cases unfolded in one or a few seismic events followed by an eruption, then SO2 emission with a magnitude often reflected by that of the eruption. If I had to rebuild this mapping tool, the only change I think would implement is the option to view the fault lines and names of major fault zones. This data is already available by clicking on the event but adding this to the visual could prove handy."
+    ]
+  ];
+
 /*document.write("Where is my JS code?");
 
 window.alert(5 + 6);
@@ -61,12 +73,12 @@ document.write(message);
 function webmap_table()
 {
   document.write("<table width=100%>");
-  for (var row=0; row < 2; row++)
+  for (var row=0; row < webmaps.length; row++)
   {
     document.write("<tr>");
-    for (var column=0; column < 3; column++)
+    for (var column=0; column < webmaps[0].length; column++)
     {
-      document.write("<td>" + row + "," + column + "</td>");
+      document.write("<td>" webmaps[row][column] + "</td>");
     }
     document.write("</tr>");
   }
